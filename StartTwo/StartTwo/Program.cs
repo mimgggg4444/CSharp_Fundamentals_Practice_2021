4bd58ba7a320376b883a513447f78558483a576d
a <a@DESKTOP-231MPSD>
2020-05-30 03:03:49 +0900

    new

--- StartTwo/StartTwo/Program.cs
+++ StartTwo/StartTwo/Program.cs
@@ -12,23 +12,23 @@ namespace PrimitiveTypesToBinary
             int int1 = int.MaxValue;
             int int2 = int.MinValue;
 
-            long long1 = long.MinValue;
-            long long2 = long.MaxValue;
+            long long1 = long.MaxValue;
+            long long2 = long.MinValue;
 
             char char1 = 'x';
             char char2 = '$';
 
-            Console.WriteLine(byte1 + " -> " + Convert.ToString(byte1, 2));
-            Console.WriteLine(byte2 + " -> " + Convert.ToString(byte2, 2));
+            Console.WriteLine(byte1 + " - > " + Convert.ToString(byte1, 2));
+            Console.WriteLine(byte1 + " - > " + Convert.ToString(byte2, 2));
 
-            Console.WriteLine(int1 + " -> " + Convert.ToString(int1, 2));
-            Console.WriteLine(int2 + " -> " + Convert.ToString(int2, 2));
+            Console.WriteLine(byte1 + " - > " + Convert.ToString(int1, 2));
+            Console.WriteLine(byte1 + " - > " + Convert.ToString(int2, 2));
 
-            Console.WriteLine(long1 + " -> " + Convert.ToString(long1, 2));
-            Console.WriteLine(long2 + " -> " + Convert.ToString(long2, 2));
+            Console.WriteLine(byte1 + " - > " + Convert.ToString(long1, 2));
+            Console.WriteLine(byte1 + " - > " + Convert.ToString(long2, 2));
 
-            Console.WriteLine(char1 + " -> " + Convert.ToString(char1, 2));
-            Console.WriteLine(char2 + " -> " + Convert.ToString(char2, 2));
+            Console.WriteLine(byte1 + " - > " + Convert.ToString(char1, 2));
+            Console.WriteLine(byte1 + " - > " + Convert.ToString(char2, 2));
         }
     }
 }

c503c5587f5648cf42d6624d1806a2c9f57a8839
a <a@DESKTOP-231MPSD>
2020-05-30 02:53:53 +0900

    Create solution.

---
+++ StartTwo/StartTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ startOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace startOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

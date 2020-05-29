bcafcbc259b83be9516c46fb89f34694aef13a3d
a <a@DESKTOP-231MPSD>
2020-05-30 03:44:57 +0900

    Comment

---
+++ startThree/Comment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Comment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            //nananananana
+
+            /*
+             nanananan
+            na
+            nanan
+
+             */
+        }
+    }
+}

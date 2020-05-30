bf6d6725c8243b61e8c4950e2ae4bf8e95d17254
a <a@DESKTOP-231MPSD>
2020-05-30 21:41:38 +0900

    코멘트 추가

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -9,9 +9,11 @@ namespace PartialClass
             string name = "Lulu";
             int id = 202036306;
 
+            //방법 1
             string message1 = string.Format("Student Name: {0} / Student ID: {1}", name, id);
             Console.WriteLine(message1);
 
+            //방법 2
             Console.WriteLine("Student Name: {0} / Student ID: {1}", name, id);
 
         }

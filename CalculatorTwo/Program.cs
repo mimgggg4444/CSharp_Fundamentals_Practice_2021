7be90abab682db6b4e1c10658f779df2b2ed5883
a <a@DESKTOP-231MPSD>
2020-05-31 00:09:52 +0900

    CalculatorTwo

---
+++ CalculatorTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculatorTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            if(op=="+")
+            {
+                Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            }
+        }
+    }
+}

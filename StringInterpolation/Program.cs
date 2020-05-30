7faa48812526121f537699a19f67e98bbacb4c82
a <a@DESKTOP-231MPSD>
2020-05-30 21:41:03 +0900

    string interpolation

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PartialClass
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lulu";
+            int id = 202036306;
+
+            string message1 = string.Format("Student Name: {0} / Student ID: {1}", name, id);
+            Console.WriteLine(message1);
+
+            Console.WriteLine("Student Name: {0} / Student ID: {1}", name, id);
+
+        }
+    }
+}

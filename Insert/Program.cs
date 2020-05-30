58ae26e0c87b00f4bbdf417eabc8491e800b040b
a <a@DESKTOP-231MPSD>
2020-05-30 22:51:00 +0900

    Insert

---
+++ Insert/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Insert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's fill out student information: ");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Student Number: ");
+            int studentNumbeString = int.Parse(Console.ReadLine());
+
+            Console.Write("Grade: ");
+            int grade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"{name}\n{studentNumbeString}\n{grade}");
+        }
+    }
+}

d4b39f06b0cd505c51afdf3b2c37d910c4d50ffb
a <a@DESKTOP-231MPSD>
2020-05-31 06:46:15 +0900

    test

---
+++ test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            if(num1 == 10 || ++num2 == 20)
+            {
+                ++num1;
+            }
+
+            Console.WriteLine($"{num1} {num2}");
+        }
+    }
+}

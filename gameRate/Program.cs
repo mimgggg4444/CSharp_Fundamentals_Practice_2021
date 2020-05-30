c7046ca85c6385d0d47abcae93b0a9940bb9def8
a <a@DESKTOP-231MPSD>
2020-05-30 22:13:10 +0900

    gameRate

--- gameRate/Program.cs
+++ gameRate/Program.cs
@@ -16,6 +16,12 @@ namespace gameRate
             Console.WriteLine("{0,10} {1,15}", studentName1, winRate1);
             Console.WriteLine("{0,10} {1,15}", studentName2, winRate2); // 왼쪽 정렬을 하려면 음수를 사용하면 됨.
 
+            //방법1
+            string message1 = string.Format($"Student Name: {studentName2} / Student ID {winRate2}");
+            Console.WriteLine(message1);
+            //방법2
+            Console.WriteLine($"Student Name: {studentName1} / Student ID: {winRate1}");
+
             Console.WriteLine("{0:f2}", 3.14159265359);             // 소수점을 사용하려는 경우{0:f2}
                                                                     // 반올림이 된다 
 

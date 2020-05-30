1c0dd961b4d039c010cc2c4520137e6458e66da3
a <a@DESKTOP-231MPSD>
2020-05-30 21:51:40 +0900

    gameRate

---
+++ gameRate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace gameRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName1 = "kim";
+            float winRate1 = 30.2351f;
+
+            string studentName2 = "oh";
+            float winRate2 = 70.11f;
+
+            Console.WriteLine("{0} {1}\n", "name", "win Rate");
+            Console.WriteLine("{0} {1}", studentName1, winRate1);
+            Console.WriteLine("{0} {1}", studentName2, winRate2);
+        }
+    }
+}

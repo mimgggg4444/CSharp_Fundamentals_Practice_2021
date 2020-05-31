0cb2085970e1954d429cc21e046b2bf7f12deb7b
a <a@DESKTOP-231MPSD>
2020-05-31 10:28:33 +0900

    InsertStudentInformation2

--- InsertStudentInformation2/Program.cs
+++ InsertStudentInformation2/Program.cs
@@ -6,34 +6,51 @@ namespace InsertStudentInformation2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's fill out student grade: ");
+            Console.WriteLine("Let's fill out student information: ");
+            string[] names = new string[3];
+            string[] studentNumbers = new string[3];
+            int[] grades = new int[3];
+
+            Console.WriteLine("Fill out information for student1.");
 
             Console.Write("Name: ");
-            string name = Console.ReadLine();
+            names[0] = Console.ReadLine();
+
+            Console.Write("student Number: ");
+            studentNumbers[0] = Console.ReadLine();
 
             Console.Write("Grade: ");
-            int grade = int.Parse(Console.ReadLine());
+            grades[0] = int.Parse(Console.ReadLine());
 
-            if(grade >= 90)
-            {
-                Console.Write("Excellent! You are an A student!");
-            }
-            else if(grade >= 80)
-            {
-                Console.Write("Good! You are above average!");
-            }
-            else if(grade >= 70)
-            {
-                Console.Write("Meh... You are doing alright...");
-            }
-            else if(grade >= 50)
-            {
-                Console.Write("are you even trying? You can do better than that!");
-            }
-            else
-            {
-                Console.Write("You have no talent... Maybe you should do something else?");
-            }
+            Console.WriteLine("Fill out information for student2.");
+
+            Console.Write("Name: ");
+            names[1] = Console.ReadLine();
+
+            Console.Write("student Number: ");
+            studentNumbers[1] = Console.ReadLine();
+
+            Console.Write("Grade: ");
+            grades[1] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fill out information for student2.");
+
+            Console.Write("Name: ");
+            names[2] = Console.ReadLine();
+
+            Console.Write("student Number: ");
+            studentNumbers[2] = Console.ReadLine();
+
+            Console.Write("Grade: ");
+            grades[2] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("1 {0,-20} {1,-20} {2}%", names[0], studentNumbers[0], grades[0]);
+            Console.WriteLine("1 {0,-20} {1,-20} {2}%", names[1], studentNumbers[1], grades[1]);
+            Console.WriteLine("1 {0,-20} {1,-20} {2}%", names[2], studentNumbers[2], grades[2]);
+
+
+
         }
     }
 }

508a985d294596ed7d2b8d38976f215bc4378629
a <a@DESKTOP-231MPSD>
2020-05-31 09:26:49 +0900

    CalculatorWithSwitchStatement

--- CalculatorWithSwitchStatement/Program.cs
+++ CalculatorWithSwitchStatement/Program.cs
@@ -6,6 +6,30 @@ namespace CalculatorWithSwitchStatement
     {
         static void Main(string[] args)
         {
+            Console.Write("num1: ");
+            long num1 = long.Parse(Console.ReadLine());
+
+            Console.Write("num2: ");
+            long num2 = long.Parse(Console.ReadLine());
+
+            Console.Write("operation(+,-,*,/): ");
+            string operation = Console.ReadLine();
+
+            switch(operation)
+            {
+                case "+":
+                    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                    break;
+            }
             
         }
     }

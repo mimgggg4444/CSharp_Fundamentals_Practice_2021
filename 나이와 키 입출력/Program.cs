86c3e0f6c2cf7014eba02bac5afaeb4630578522
a <a@DESKTOP-231MPSD>
2020-05-31 09:57:11 +0900

    age and height input and output using array

--- 나이와 키 입출력/Program.cs
+++ 나이와 키 입출력/Program.cs
@@ -6,25 +6,33 @@ namespace 나이와_키_입출력
     {
         static void Main(string[] args)
         {
+            int[] ages = new int[3];
+            float[] heights = new float[3];
+
             Console.Write("Enter the first user's age: ");
-            int age1 = int.Parse(Console.ReadLine());
+            ages [0] = int.Parse(Console.ReadLine());
             Console.Write("Enter the first user's height(cm): ");
-            float height1 = float.Parse(Console.ReadLine()); 
+            heights [0] = float.Parse(Console.ReadLine()); 
 
             Console.Write("Enter the second user's age: ");
-            int age2 = int.Parse(Console.ReadLine());
+            ages [1] = int.Parse(Console.ReadLine());
             Console.Write("Enter the second user's height(cm): ");
-            float height2 = float.Parse(Console.ReadLine());
+            heights [1] = float.Parse(Console.ReadLine());
 
             Console.Write("Enter the third user's age: ");
-            int age3 = int.Parse(Console.ReadLine());
+            ages [2] = int.Parse(Console.ReadLine());
             Console.Write("Enter the third user's height(cm): ");
-            float height3 = float.Parse(Console.ReadLine());
+            heights [2] = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Print result ----------------- ");
-            Console.WriteLine($"The first User: Age({age1}), height({height1}cm)");
-            Console.WriteLine($"The second User: Age({age2}), height({height2}cm)");
-            Console.WriteLine($"The third User: Age({age3}), height({height3}cm)");
+            Console.WriteLine($"The first User: Age({ages[0]}), height({heights[0]}cm)");
+            Console.WriteLine($"The second User: Age({ages[1]}), height({heights[1]}cm)");
+            Console.WriteLine($"The third User: Age({ages[2]}), height({heights[2]}cm)");
+
+            int totalAges = ages[0] + ages[1] + ages[2];
+            Console.WriteLine($"Total ages: {totalAges}");
+            float totalHeights = heights[0] + heights[0] + heights[1] + heights[2];
+            Console.WriteLine($"Total heihgts: {totalHeights}");
         }
     }
 }

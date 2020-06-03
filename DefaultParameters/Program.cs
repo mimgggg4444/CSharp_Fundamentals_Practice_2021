000b049793b046b38a3134d46bedb8f861076767
a <a@DESKTOP-231MPSD>
2020-06-03 12:35:20 +0900

    DefaultParameters

---
+++ DefaultParameters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace DefaultParameters
+{
+    class Program
+    {
+        static void Main(string[]agrs)
+        {
+            Bar("POCU");
+            Bar("POCU", "COMP1500");
+            Bar("POCU", "COMP1500", "Intero to Programming!");
+        }
+
+        static void Bar(string s, string s2 = "", string s3 = "")
+        {
+            Console.WriteLine($"{s}, {s2}, {s3}");
+        }
+    }
+}

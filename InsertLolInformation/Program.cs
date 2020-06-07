423e2b932ad00855e8de136efe3913a312f3f41f
a <a@DESKTOP-231MPSD>
2020-06-08 08:57:15 +0900

    InsertLolInformation

---
+++ InsertLolInformation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InsertLolInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's fill out lol information");
+
+            Console.Write("Please write your nickName: ");
+            string nickName = Console.ReadLine();
+
+            Console.Write("Please write your tier: ");
+            string tier = Console.ReadLine();
+
+            Console.Write("Please write your rate: ");
+            string rate = Console.ReadLine();
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine($"Name: {nickName}, tier: {tier}, rate: {rate}");
+        }
+    }
+}

f1d140b466ef50ede21f4ac845b69391dc6be33b
a <a@DESKTOP-231MPSD>
2020-06-08 09:04:50 +0900

    StringInterpolation1

---
+++ StringInterpolation1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringInterpolation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 48;
+            int num2 = 65;
+
+            Console.WriteLine($"The sum of {num1} and {num2} is {num1 + num2}");
+            Console.WriteLine(string.Format("The sum of {0} and {1} is {2}", num1, num2, num1 + num2));
+
+            Console.WriteLine($"\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
+            Console.WriteLine(@"\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
+
+            Console.WriteLine("{0, -11} + {1, 11} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0, -11} / {1, 11} = {2:f4}", num1, num2, num1 / (float)num2);
+            Console.WriteLine("{0, -11} % {1, 11} = {2}", num1, num2, num2 % num1); //65를 48로 나누어야 함
+
+
+        }
+    }
+}

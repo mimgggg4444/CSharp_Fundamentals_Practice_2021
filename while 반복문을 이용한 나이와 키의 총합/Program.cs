6ef393f0da2cdf3ba7fee3205bfc122ffbd4937d
a <a@DESKTOP-231MPSD>
2020-06-10 08:01:17 +0900

    while 반복문을 이용한 나이와 키의 총합을 구하는 코드

---
+++ while 반복문을 이용한 나이와 키의 총합/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace while_반복문을_이용한_나이와_키의_총합
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("user count: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] ages = new int[length];
+            float[] height = new float[length];
+
+            int totalAge = 0;
+            float totalHeight = 0.0f;
+            int count = 0;
+            while(count < length)
+            {
+                Console.Write($"Enter user{count}'s age: ");
+                ages[count] = int.Parse(Console.ReadLine());
+                totalAge += ages[count];
+
+                Console.Write($"Enter user{count}'s height: ");
+                height[count] = float.Parse(Console.ReadLine());
+                totalHeight += height[count];
+
+                ++count;
+            }
+
+            Console.WriteLine($"Total ages: {totalAge}");
+            Console.WriteLine($"Total height: {totalHeight}");
+        }
+    }
+}

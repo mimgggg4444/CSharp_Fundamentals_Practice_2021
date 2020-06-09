05e221348113bdeafeaaf843943d42817c774cba
a <a@DESKTOP-231MPSD>
2020-06-10 08:05:54 +0900

    simplelogin

---
+++ simpleLoginProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace simpleLoginProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string passcode = "3941a";
+            string userInput = "";
+        
+            while(true)
+            {
+                Console.Write("Please enter the password: ");
+                userInput = Console.ReadLine();
+
+                if(passcode == userInput)
+                {
+                    Console.WriteLine("correct! welcome!");
+                    break;
+                }
+
+                Console.WriteLine("wrong password");
+            }
+        }
+    }
+}
